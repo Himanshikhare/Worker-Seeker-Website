0e5991ab7009e135b24d3bd3a7083b9261188ff4
Frontend <117911062+Himanshikhare@users.noreply.github.com>
2025-05-03 13:40:32 +0530

    first commit

---
+++ Frontend/src/components/FilterCard.jsx
@@ -0,0 +1,53 @@
+import { setSearchedQuery } from '@/redux/jobSlice'
+import { Label } from '@radix-ui/react-label'
+import { RadioGroup, RadioGroupItem } from '@radix-ui/react-radio-group'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+const filterData=[
+    {
+        filterType: "Location",
+        array: ["Bhopal", "Indore", "Sagar", "Pune", "Mumbai"]
+    },
+    
+]
+const FilterCard=()=> {
+    const [selectedValue, setSelectedValue] = useState('');
+    const dispatch = useDispatch();
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    }
+    useEffect(()=>{
+        dispatch(setSearchedQuery(selectedValue));
+    },[selectedValue]);
+
+  return (
+    <div className='w-full bg-white p-3 rounded-md'>
+        <h1 className='font-bold text-lg'>Filter Jobs</h1>
+        <hr className='mt-3' />
+        <RadioGroup value={selectedValue} onValueChange={changeHandler}>
+                {
+                    filterData.map((data, index) => (
+                        <div>
+                            <h1 className='font-bold text-lg'>{data.filterType}</h1>
+                            {
+                                data.array.map((item, idx) => {
+                                    const itemId = `id${index}-${idx}`
+                                    return (
+                                        <div className='flex items-center space-x-2 my-2'>
+                                            <RadioGroupItem value={item} id={itemId} />
+                                            <Label htmlFor={itemId}>{item}</Label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ))
+                }
+            </RadioGroup>
+
+    </div>
+  )
+}
+
+export default FilterCard
